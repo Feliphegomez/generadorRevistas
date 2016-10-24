da1c9988cdcecc9c505ab10ac7d764f35cf87f12
Andres Felipe Gomez Maya <feliphegomez@gmail.com>
2016-10-23 22:36:18 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+	define("NAME_SERVER","localhost");
+	define("USER_SERVER","root");
+	define("PASS_SERVER","");
+	define("DBNAME_SERVER","diariofg");
+	define("TABLA_NAME","paginas");
+
+?>
